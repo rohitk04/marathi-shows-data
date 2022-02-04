be571660070b216beea357fe07b33692729f5e64
rohitk04 <rkasale28@gmail.com>
2022-02-04 13:04:45 +0530

    Added get_week function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from convert_to_csv import convert_to_csv
+from get_week import get_week
 from process import process
 from convert_to_json import convert_to_json
 
@@ -11,28 +12,23 @@ print ()
 
 ch = int(input("Enter choice: "))
 
-num = int(input("Enter week no.: "))
-week = 'Week ' + str(num)
-
-print ()
-print ('-'.center(73,'-'))
-print ("\nEntering data for " + week + "...")
-print ()
-print ('-'.center(73,'-'))
-
 if (ch==1):
+    week = get_week()
     convert_to_json("TV", week)
     process("TV", 'data/json/shows/shows_backup.json', 'data/json/shows/shows.json', 'shows')
     convert_to_csv ("TV")
 elif (ch==2):
+    week = get_week()
     convert_to_json("Mahaepisode", week)
     process("Mahaepisode", 'data/json/shows/mahaepisode/mahaepisodes_backup.json', 'data/json/shows/mahaepisode/mahaepisodes.json', 'shows')
     convert_to_csv("Mahaepisode")
 elif (ch==3):
+    week = get_week()
     convert_to_json("Online", week)
     process("Online", 'data/json/shows/shows_backup.json', 'data/json/shows/shows.json', 'shows')
     convert_to_csv("Online")
 elif (ch==4):
+    print ('-'.center(73,'-'))
     process("Channel", 'data/json/channels/channels_backup.json', 'data/json/channels/channels.json', 'channels')
     convert_to_csv("Channel")
 else:
