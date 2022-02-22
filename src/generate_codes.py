73bbf5126ee3ece8e0c3793f93411721cabe86df
rohitk04 <rkasale28@gmail.com>
2022-02-22 19:47:22 +0530

    Codes get generated directly from show names

--- src/generate_codes.py
+++ src/generate_codes.py
@@ -4,18 +4,15 @@ import pandas as pd
 from output import store_into_json
 
 df = pd.DataFrame(columns=['Hashtag','Code'])
-codes = json.load(open('data/json/shows/codes.json'))
 
 with open('data/trp.txt') as f:
     lines = f.readlines()
 
-dictionary = {}
+num = 1
 
 for line in lines:
-    words = line.split(" ")
-    if words[1] not in codes.keys():
-        code = ''.join(' '+char if char.isdigit() or char.isupper() else char.strip() for char in words[1].lstrip('#')).strip()
-        dictionary[words[1]] = code
-
-codes.update(dictionary)
-store_into_json('data/json/shows/codes.json', codes)
+    words = line.split('-')
+    show = words[0].replace(' ','').strip()
+    new_line = str(num) + ". #" + show + ' - '
+    print (new_line)
+    num = num + 1
