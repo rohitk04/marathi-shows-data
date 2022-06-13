e33d9c3dad818946e61bf9275538503f17ce2360
rohitk04 <rkasale28@gmail.com>
2022-06-13 19:43:22 +0530

    Added new channel in channel choices

--- data/choices/channel_choices.py
+++ data/choices/channel_choices.py
@@ -1,6 +1,7 @@
 channel_choices = {
-    1:'Star Pravah', 
-    2:'Zee Marathi', 
-    3:'Colors Marathi', 
-    4:'Sony Marathi'
+    1: 'Star Pravah', 
+    2: 'Zee Marathi', 
+    3: 'Colors Marathi', 
+    4: 'Sony Marathi',
+    5: 'Sun Marathi'
 }
